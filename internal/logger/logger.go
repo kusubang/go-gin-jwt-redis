737a1be1da11b8964bb744e1e50282a5fce296cf
Yeonheon Gu <kusubang@modutech.co.kr>
2022-03-30 17:48:43 +0900

    add logrus basic

---
+++ internal/logger/logger.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+)
+
+var log *logrus.Logger
+
+func init() {
+	log = &logrus.Logger{
+		Out:   os.Stderr,
+		Level: logrus.DebugLevel,
+		Formatter: &prefixed.TextFormatter{
+			DisableColors:   true,
+			TimestampFormat: "2006-01-02 15:04:05",
+			FullTimestamp:   true,
+			ForceFormatting: true,
+		},
+	}
+}
+
+func GetLogger() *logrus.Logger {
+	return log
+}
--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
-	"github.com/kusubang/auth/internal/routes"
+	"github.com/kusubang/auth/internal/logger"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 func main() {
+
+	log := logger.GetLogger()
+
+	log.SetReportCaller(true)
+	log.WithFields(logrus.Fields{
+		"user": "admin",
+	}).Info("Some interesting info")
+
+	log.Warn("This is a warning")
+	log.Error("An error occured!")
+
 	viper.SetConfigFile(".env")
 	viper.ReadInConfig()
-	r := routes.SetupRouter()
-	r.Run(":" + viper.GetString("PORT"))
+	// r := routes.SetupRouter()
+	// r.Run(":" + viper.GetString("PORT"))
 }
